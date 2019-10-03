a429795670bf6efb3f5ad21123aa3b0a919faa16
Matt Maggio <mattmaggio19@gmail.com>
2019-10-03 16:57:26 -0500

    Worked on decomposing the ETCO2 and arterial signals.

--- SA1PhysiologyTools.py
+++ SA1PhysiologyTools.py
@@ -3,6 +3,7 @@ import numpy as np
 import time
 import GazmuriLoader as GL
 import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
 import SA1DataLoader
 import os, pickle
 
@@ -71,9 +72,7 @@ def DenoiseSignal(Series, SignalType = 'Hemodyanmic'):
 
     #start by calculating the zscore for the whole trace. the standard dev may also be informative.
 
-    #First zero out any data above 150 and below -10, this should tighen our std range significantly. Maybe doesn't work for LV?
-    # filter1, filter2,  = Series > -10, Series < 150
-    # Series = Series.where(filter1 & filter2)
+    #First nan out any data above 150 and below -10, this should tighen our std range significantly. Maybe doesn't work for LV?
 
     #
 
@@ -86,30 +85,34 @@ def DenoiseSignal(Series, SignalType = 'Hemodyanmic'):
 
     Series_zscore = (Series - SeriesMean) / SeriesStd
 
-    SeriesRoll = Series.rolling(30*250).mean()
+    SeriesRoll = Series.rolling(10*250).mean()
     RollDiff = SeriesRoll.diff()
     diffZ = (RollDiff - RollDiff.mean())/RollDiff.std()
 
 
-    Threshold = 3 # a zscore above 5 usually means a flush. Works pretty well for acute.
+    Threshold = 5 # a zscore above 5 usually means a flush. Works pretty well for acute.
     # #I kind of don't like the zscore thing. lets try an approach based on the d.
 
     filter3 = diffZ.abs() < Threshold
     Series = Series.where(filter3)
 
     Padding = 20*250
+
+    filter1, filter2,  = Series > -10, Series < 150
+    Series = Series.where(filter1 & filter2)
+
     CensorSeries = Series.copy()
 
-    # Censor any data that is within 10 seconds of a detected bad point.
+    # Censor any data that is within 20 seconds of a detected bad point.
     for ix, value in Series.items(): #TODO make faster by instead iterating only though the places where we are not below the threshold.
-        if np.isnan(value) and not all(CensorSeries.iloc[ix-Padding:ix+Padding-1] == np.nan):
+        if np.isnan(value) and not all(CensorSeries.iloc[ix-Padding:ix-1] == np.nan):
             # print('removed data around the point {0}'.format(ix))
             CensorSeries.iloc[ix-Padding:ix] = np.nan #Apparently it is quite bad to change the thing you are iterating over proformance wise.
 
     # plt.plot(Series)
-    plt.plot(CensorSeries)
-    plt.plot((filter3)*SeriesMean)
-    plt.show()
+    # plt.plot(CensorSeries)
+    # plt.plot((filter3)*SeriesMean)
+    # plt.show()
 
     return CensorSeries
 
@@ -174,12 +177,14 @@ def MapBeforeDeathEvent(useCashe = False, Timebefore = 10):
             DeathLst.append(exp)
     print(DeathLst)
 
+    fig = plt.subplot(np.ceil(len(DeathLst)/2), 2, 1)
+
     for ix, exp in enumerate(DeathLst):
         #load the experiment into memory, should load only the files that go upto and contain the death time.
         df = GL.LoadRawExperiment(SA1Raw[exp], TimeTup=(0, Deathdict[exp]))
 
         #How many minitues before death you want to look at.
-        BeforeDeathFrame = 15
+        BeforeDeathFrame = 30
 
         # Extract the features of intrest.
         Deathidx = int(round(Deathdict[exp]) * 250) #Round to the nearest second convert to a point index.
@@ -187,15 +192,23 @@ def MapBeforeDeathEvent(useCashe = False, Timebefore = 10):
 
         Ao = df.iloc[:, 1]
         AoDenoised = DenoiseSignal(Ao) #Maybe, Only denoise the signal that we want to use for the thing, faster, but causes us to censor less of the flushes.
-        AoAveraged = AoDenoised.rolling(250*60).mean()    #1 min mean
-        Etco2 = df.iloc[:, 10].rolling(250*60).mean()  #1 min mean
+        AoAveraged = AoDenoised.rolling(250*10).mean()    #10 sec mean
+        Etco2 = df.iloc[:, 10].rolling(250*10).mean()  #1 min mean
         Time = df['Time']
 
+        Ax =  plt.subplot( np.ceil(len(DeathLst) / 2), 2, ix+1, frameon=True)
+        Ax.set_title(exp)
+        Ax.plot((Time.iloc[idx]-(Deathidx/250))/60, Etco2.iloc[idx], label='Mean Etco2 value')
+        Ax.plot((Time.iloc[idx]-(Deathidx/250))/60, AoAveraged.iloc[idx], label='Mean Art pressure')
 
+        Ax.set_yticks(np.arange(0, 60, step=10))
+        Ax.set_ylim(0, 60)
+        fontP = FontProperties()
+        fontP.set_size('small')
+        Ax.legend(loc='lower left', bbox_to_anchor=(-0.25, -0.25), prop=fontP)
 
-        plt.plot(Time.iloc[idx], AoAveraged.iloc[idx])
-        plt.plot(Time.iloc[idx], Etco2.iloc[idx])
-        plt.show()
+    plt.tight_layout()
+    plt.show()
 
         # Graph the Ao signal with a 1 min average MAP, 1 min average end tidal
 
@@ -211,39 +224,94 @@ def MapBeforeDeathEvent(useCashe = False, Timebefore = 10):
         #     print('exp number {0} Death time from automatic detection {1}  '
         #           'vs  {2}   from the excel sheet '.format(expNum, Deathdict[expNum] / 60, exp['Survival time']))
 
+def DecomposeEtCo2Signal(ETCO2 , Return = 'Corrected' ):
+    #Depending on the user to feed the correct series.
+    t = time.time()
+
+    #Run a 1 min rolling average and subtract the Series from it. Then mask out the positive portion, average the positive and negative portion and return the corrected trace.
+    AveETCO2 = ETCO2.rolling(250*20).mean()
+    ZeroedETCO2 = ETCO2 - AveETCO2
+
+    #This is a clever filter because you use the local maximum as the cutoff to eliminate most of the artifact from the upswing downswing.
+    ExhaledETCO2Filter = ZeroedETCO2 > 0.7 * ZeroedETCO2.rolling(250*20).max()
+    InhaledETCO2Filer = ZeroedETCO2 < 0.7 * ZeroedETCO2.rolling(250*20).min()
+
+    if Return == 'Corrected':
+        #cython approach is almost required for data of this size.
+        ExhaledETCO2 = ETCO2.where(ExhaledETCO2Filter).rolling(20 * 250, min_periods=1, center=True).mean()
+        InhaledETCO2 = ETCO2.where(InhaledETCO2Filer).rolling(20 * 250, min_periods=1, center=True).mean()
+        CorrectedETCO2 = ExhaledETCO2 - InhaledETCO2
+        t1 = time.time()
+        print('Took {0} to process {1} seconds of data '.format(str(t1 - t), str(ETCO2.shape[0]/250)))
+
+        #Diagnotic plotting code.
+        plt.plot(ETCO2, label = 'Raw ETCO2 signal')
+        plt.plot(ExhaledETCO2, label = 'Exhaled')
+        plt.plot(InhaledETCO2, label = 'Inhaled')
+        plt.plot(CorrectedETCO2, color='red', label = 'Corrected')
+        # plt.plot(CorrectedETCO2First)
+        plt.legend()
+        plt.show()
+        return CorrectedETCO2
+
+    elif Return == 'filters': #Much faster.
+        #If we want just the filters, we are much more likely intrested in a multually exclusive filter that transitions during inhilation and exhilation.
+        ExhaledETCO2Filter = ZeroedETCO2 > 0
+        InhaledETCO2Filer = ~ExhaledETCO2Filter
+        return (ExhaledETCO2Filter, InhaledETCO2Filer)
 
 
-def DecomposeArterialSignal(df=None, window=10, sampleingFreq=250, rollingOrWindow = 'window', AoChannel = 1):
 
-    #Given an arterial signal find the systolic, diastolic, and mean after considering the effect of the breathing artifact.
-    Ao = df.iloc[:, 1]
-    Time = df['Time']
 
-    #Calculate the mean, and standard dev of the whole trace. This will let us throw outliers out and drop things like flushes.
-    AoMean = Ao.mean()
-    AoStd = Ao.std()
 
-    Ao_zscore =  (Ao - AoMean) / AoStd
-    idx = slice(250*60*55, 250*60*240)
-    plt.plot(Ao.iloc[idx].index/(250*60), Ao.iloc[idx].values)
+def MomentOfCCIAnalysis(dataset):
+    #TODO This function should parse the dataset and isolate the small period around the CCI impact.
+    #We are concerned with the phase of the resp and cardiac cycle that we are in when the impact happens.
+    #At some point we might try to see if ICP spike hieght is related to the dura rupture event or if it is independant.
+    pass
+
+def DecomposeArterialSignal(Ao, Return = 'all'):
+    #Starting work on the arterial signal
+
+    #Start with a moving average of the signal for like a 10 seconds.
+    AveAo = Ao.rolling(250 * 10).mean()
+    ZeroedAo = Ao - AveAo
+
+    #Count the cumulative number of transitions per time as the heart rate.
+    #Convert to numpy for this, the solutions just don't seem as elegant in pandas.
+    ZeroedAoArray = ZeroedAo.to_numpy()
+
+    BeatTrace = np.where(np.diff(np.signbit(ZeroedAoArray)))[0] + Ao.index[0] #sync with the experimental time.
+
+    # plt.plot(Ao)
+    plt.plot(ZeroedAo)
+    plt.vlines(BeatTrace, ymin=-20, ymax=20)
+    #detecting the dichrotic notch, need to eliminate points that are too close to other points.
+    #High quality Ao signal is needed.
 
     plt.show()
-    #As an additional thing, if you can locate the dicrotic notch, calculate the stroke volume.
 
+    # print(BeatTrace)
+    # print(len(BeatTrace))
+    # print(len(BeatTrace))
 
-    #We cannot output these values at every timepoint so there has to be some kind of window at which a new score is determined.
-
+    pass
 
 
 if __name__ == '__main__':
+    # MapBeforeDeathEvent(useCashe=True)
 
     SA1Raw = GL.SA1standardRAWloadingFunction(useCashe=False)
 
-    df = GL.LoadRawExperiment(SA1Raw['2018106'], TimeTup=(0, 30))
+    df = GL.LoadRawExperiment(SA1Raw['2018108'], TimeTup=(0, 60))
 
     Ao = df.iloc[:, 1]
+    # ETCO2 = df.iloc[:, 10]
+    # ETCO2Corrected = DecomposeEtCo2Signal(ETCO2.iloc[0:400*250])
 
-    FFTSignalAnalysis(Ao.iloc[300*250:360*250])
+    DecomposeArterialSignal(Ao[30*250*60:40*250*60])
+
+    # FFTSignalAnalysis(Ao.iloc[300*250:360*250])
 
 
     # MapPreDeath = MapBeforeDeathEvent(useCashe = True)
